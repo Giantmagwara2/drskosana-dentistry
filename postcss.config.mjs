1f2d69969b8157dc71496cad292d083bfa5cb83b
Giantmagwara2 <giantmagwara2@gmail.com>
2025-04-06 09:27:45 +0200

    Fix: update PostCSS config to use CommonJS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
-
-export default {
+module.exports = {
   plugins: [
-    tailwindcss,
-    autoprefixer,
-    ...(process.env.NODE_ENV === 'production'
-      ? [cssnano({ preset: 'default' })]
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
+      ? [require("cssnano")({ preset: "default" })]
       : []),
   ],
 };
