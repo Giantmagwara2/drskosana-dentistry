483e25ada414b38303d15561d212060950fbea5c
Giantmagwara2 <giantmagwara2@gmail.com>
2025-04-07 17:02:09 +0200

    Update PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {}, // Use the new Tailwind CSS PostCSS plugin
+    autoprefixer: {}, // Add other PostCSS plugins if needed
     ...(process.env.NODE_ENV === "production"
       ? { cssnano: { preset: "default" } }
       : {}),
